bd43ab841af2565f3361087f83d79e6b81572a5a
Felipe Molina <felipemolina.contato@gmail.com>
2026-04-26 20:50:51 -0300

    chore: remover arquivos e scripts não utilizados do projeto

--- scripts/autoload/event_bus.gd
+++
@@ -1,9 +0,0 @@
-extends Node
-
-signal entity_selected(entity: Node)
-signal entity_deselected()
-signal entity_placed(entity: Node, cell: Vector2i)
-signal build_mode_changed(active: bool, entity_id: String)
-signal game_saved(path: String)
-signal game_loaded(path: String)
-signal cell_hovered(cell: Vector2i)
--- scripts/autoload/game_manager.gd
+++
@@ -1,9 +0,0 @@
-extends Node
-
-var current_save_path: String = "user://save_game.json"
-
-func save_game(path: String = current_save_path) -> void:
-	pass
-
-func load_game(path: String = current_save_path) -> void:
-	pass
